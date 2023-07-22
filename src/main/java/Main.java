fbf433ea28fc6933cbf0a41c1987c0e90a9b846b
ron bar yosef <ronibary@gmail.com>
2023-07-22 17:33:36 +0300

    changing greet message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello everyone have a great week all :)");
+        System.out.println("Hello everybody have a great week all  :)");
 
     }
 
